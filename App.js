85defd02707d6eede7684d1cf274c5bfe0683efc
aadarshSrivasatv <aadarsh.s@antino.io>
2023-03-24 11:47:16 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+const App = () => {
+  return (
+    <View>
+      <Text>hello aadarsh</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
+export default App;
